5e30cd717e321e9303c305f2cd75fbaf12821a86
nongparanai <promlerd.p@gmail.com>
2023-12-20 16:02:08 +0700

    feat: addcompleted exercises

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,155 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error-alert") {
+    return (
+      <div
+        css={css`
+          width: 650px;
+          height: 80px;
+          border-radius: 10px;
+          background-color: rgba(249, 200, 200, 1);
+          color: rgb(68, 68, 68);
+          display: flex;
+          flex-direction: row;
+          margin: 20px;
+          font-family: "kanit" . sans-serif;
+          font-size: 20px;
+          font-weight: 700;
+        `}
+      >
+        <img
+          src="./src/icons/error-frown-face.svg"
+          width="24px"
+          height="24px"
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        />
+        <div
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        >
+          This is error alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "warning-alert") {
+    return (
+      <div
+        css={css`
+          width: 650px;
+          height: 80px;
+          border-radius: 10px;
+          background-color: rgba(249, 217, 200, 1);
+          color: rgb(68, 68, 68);
+          display: flex;
+          flex-direction: row;
+          margin: 20px;
+          font-family: "kanit" . sans-serif;
+          font-size: 20px;
+          font-weight: 700;
+        `}
+      >
+        <img
+          src="./src/icons/waring-triangle.svg"
+          width="24px"
+          height="24px"
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        />
+        <div
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        >
+          This is warning alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "info-alert") {
+    return (
+      <div
+        css={css`
+          width: 650px;
+          height: 80px;
+          border-radius: 10px;
+          background-color: rgba(249, 235, 200, 1);
+          color: rgb(68, 68, 68);
+          display: flex;
+          flex-direction: row;
+          margin: 20px;
+          font-family: "kanit" . sans-serif;
+          font-size: 20px;
+          font-weight: 700;
+        `}
+      >
+        <img
+          src="./src/icons/alert-circle.svg"
+          width="24px"
+          height="24px"
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        />
+        <div
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        >
+          This is info alert box
+        </div>
+      </div>
+    );
+  } else if (props.type === "success-alert") {
+    return (
+      <div
+        css={css`
+          width: 650px;
+          height: 80px;
+          border-radius: 10px;
+          background-color: rgba(206, 247, 205, 1);
+          color: rgb(68, 68, 68);
+          display: flex;
+          flex-direction: row;
+          margin: 20px;
+          font-family: "kanit" . sans-serif;
+          font-size: 20px;
+          font-weight: 700;
+        `}
+      >
+        <img
+          src="./src/icons/check-circle.svg"
+          width="24px"
+          height="24px"
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        />
+        <div
+          css={css`
+            margin-top: 30px;
+            margin-left: 15px;
+          `}
+        >
+          This is success alert box
+        </div>
+      </div>
+    );
+  } else {
+    return;
+  }
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,49 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div>
+        <button
+          css={css`
+            margin: 20px;
+            width: 170px;
+            height: 50px;
+            background-color: rgba(7, 78, 232, 1);
+            font-size: 16px;
+            border-radius: 4px;
+            color: white;
+            font-weight: bold;
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div>
+        <button
+          css={css`
+            margin: 20px;
+            width: 170px;
+            height: 50px;
+            background-color: rgba(7, 164, 232, 1);
+            font-size: 16px;
+            border-radius: 4px;
+            color: white;
+            font-weight: bold;
+          `}
+        >
+          Button
+        </button>
+      </div>
+    );
+  } else {
+    return;
+  }
+}
+
+export default Button;
